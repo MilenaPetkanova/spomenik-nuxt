25d6acc9e09e4a9c8e6b06a2386d92a27e203fca
Milena Petkanova <milenapetkanova@gmail.com>
2022-07-18 20:15:23 +0300

    create design system page

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,6 +42,7 @@ export default {
       '~/components/auth',
       '~/components/letters',
       '~/components/gallery',
+      '~/components/design-system',
     ]
   },
 
---
+++ pages/design-system.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="page is-design-system">
+    <div class="page__conatiner container py-8">
+        <Typography :nameProp="typographyConst.Header1"></Typography>
+		<Typography :nameProp="typographyConst.Header2"></Typography>
+		<Typography :nameProp="typographyConst.Header3"></Typography>
+		<Typography :nameProp="typographyConst.Subtitle"></Typography>
+		<Typography :nameProp="typographyConst.Body"></Typography>
+		<Typography :nameProp="typographyConst.Emphasis"></Typography>
+		<Typography :nameProp="typographyConst.Bold"></Typography>
+		<Typography :nameProp="typographyConst.Small"></Typography>
+		<Typography :nameProp="typographyConst.PreTitle"></Typography>
+		<Typography :nameProp="typographyConst.ButtonText"></Typography>
+		<Typography :nameProp="typographyConst.Link"></Typography>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default{
+  name: 'DesignSystem',
+  transition: 'page-transition',
+  head() {
+    return {
+      title: 'Design System',
+    };
+  },
+  computed: {
+    ...mapGetters("constants", ["typographyConst"]),
+  }
+}
+</script>
