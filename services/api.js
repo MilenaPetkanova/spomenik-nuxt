603744b40462ef3c4d00d71ef5450c6f1aeae184
Milena Petkanova <milenapetkanova@gmail.com>
2023-02-16 18:48:38 +0200

    update api url

--- services/api.js
+++ services/api.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export default () => {
   return axios.create({
-    baseURL:  process.env.NODE_ENV !== 'production' ?  'http://localhost:8000' : 'https://spomenik-api.milenapetkanova.com'
+    baseURL:  process.env.NODE_ENV !== 'production' ? process.env.SPOMENIK_API_URL_LOCAL : process.env.SPOMENIK_API_URL_PROD
   })
 }
