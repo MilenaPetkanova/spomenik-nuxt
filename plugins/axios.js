d68c8e14c7a259dab2615455fd9bce39c93eb6d7
Milena Petkanova <milenapetkanova@gmail.com>
2023-02-21 17:10:57 +0200

    delete old logic from store, plugins and middleware

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,12 +1,6 @@
 export default function({ store, app: { $axios }, redirect }) {
-  $axios.onRequest((config) => {
-    if (store.state.auth.accessToken) {
-      config.headers.Authorization = 'Bearer ' + store.state.auth.accessToken;
-    }
-    return config;
-  });
-
   $axios.onError(error => {
+    console.log('axios.js error :>> ', error);
     if (error.response.status === 500) {
       redirect('/errors/500')
     }
